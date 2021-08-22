a6d279b6e53b356ec92b0868103cba3b65afd78e
herkom <omar.guadarrama@rocketmail.com>
2021-08-22 18:07:44 -0500

    Imagenes añadidas. Falta: Optimizar imagenes para distintos dispositivos y tamaños. Estilos a los modales. Estilos responsive. Terminar los estilos del resume.

--- GalleryItem.js
+++ GalleryItem.js
@@ -3,26 +3,9 @@ app.component('galleyItem',{
         <article class="item" v-on:click="modalToggle">
             <figure class="item-thumbnail">
                 <picture>
-                    <!-- <source srcset="
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_lg_@3x.png 3x,
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_lg_@2x.png 2x,
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_lg.png 1x"
-                        media="(min-width:1000px)">
-                    <source srcset="
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_md_@3x.png 3x,
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_md_@2x.png 2x,
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_md.png 1x"
-                        media="(min-width:600px)">
-                    <img srcset="
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_sm_@3x.png 3x,
-                        ./imagenesPrueba/www.osfetabasco.gob.mx__menu_sm_@2x.png 2x"
-                        src="./imagenesPrueba/www.osfetabasco.gob.mx__menu_sm.png"
-                        alt="Screenshot del sitio oficial del OSFE Tabasco donde se ven los diferentes niveles de submenus."></img> -->
-                    <img
-                    :src=" './resized/' + item.images[0]"
-                    alt="Screenshot del sitio oficial del OSFE Tabasco donde se ven los diferentes niveles de submenus.">
+                    <img :src=" './resized/' + item.images[0]" alt="item.description">
                 </picture>
-                <figcaption>Sitio del OSFE con su menú desplegado</figcaption>
+                <figcaption>{{item.description}}</figcaption>
             </figure>
             <div class="item-info">
                 <h4>{{item.name}}</h4>
@@ -30,10 +13,37 @@ app.component('galleyItem',{
         </article>
 
 
-        <article v-on:click="modalToggle" :class="isOpen ? 'modalOpened' : 'modalClosed'">
-            <figure class="item-thumbnail">
-                <picture>
-                    <!-- <source srcset="
+        <div v-on:click="modalToggle" :class="isOpen ? 'modalOpened' : 'modalClosed'">
+            <article>
+                <figure class="item-thumbnail">
+                    <picture>
+                        <img :src=" './resized/' + item.images[0]" alt="item.description">
+                    </picture>
+                    <figcaption>{{item.description}}</figcaption>
+                </figure>
+
+                <h2>{{item.name}}</h2>
+                <h3>{{item.description}}</h3>
+
+                <p v-for="(p, index) in item.explanation">
+                    {{item.explanation[index]}}
+                </p>
+
+                <figure v-for="(image, index) in item.images">
+                    <picture>
+                        <img :src=" './resized/' + item.images[index]" alt="">
+                    </picture>
+                </figure>
+
+                <p v-for="(p, index) in item.conclusion">
+                    {{item.conclusion[index]}}
+                </p>
+            </article>
+        </div>
+
+
+
+        <!-- <source srcset="
                         ./imagenesPrueba/www.osfetabasco.gob.mx__menu_lg_@3x.png 3x,
                         ./imagenesPrueba/www.osfetabasco.gob.mx__menu_lg_@2x.png 2x,
                         ./imagenesPrueba/www.osfetabasco.gob.mx__menu_lg.png 1x"
@@ -48,13 +58,6 @@ app.component('galleyItem',{
                         ./imagenesPrueba/www.osfetabasco.gob.mx__menu_sm_@2x.png 2x"
                         src="./imagenesPrueba/www.osfetabasco.gob.mx__menu_sm.png"
                         alt="Screenshot del sitio oficial del OSFE Tabasco donde se ven los diferentes niveles de submenus."></img> -->
-                    <img
-                    :src=" './resized/' + item.images[0]"
-                    alt="Screenshot del sitio oficial del OSFE Tabasco donde se ven los diferentes niveles de submenus.">
-                </picture>
-                <figcaption>Sitio del OSFE con su menú desplegado</figcaption>
-            </figure>
-        </article>
     `,
 
     props: ['item'],
@@ -64,7 +67,6 @@ app.component('galleyItem',{
 
         function modalToggle() {
             isOpen.value = !isOpen.value;
-            console.log('que demonios')
         }
         return { 
             modalToggle,
