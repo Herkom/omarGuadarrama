658242b5f9811924ea2894c3b913ec44590ba990
herkom <omar.guadarrama@rocketmail.com>
2021-09-17 12:08:11 -0500

    Resume turned to pdf

--- Resume.js
+++ Resume.js
@@ -37,7 +37,7 @@ app.component("Resume", {
             </div>
             <h2>Diseñador UX <strong>&</strong> Desarrollador Front-End</h2>
 
-            <a download href="./OmarGuadarrama___resume.png" target="_blank" class="downloadIcon">
+            <a download href="./OmarGuadarrama___resume.pdf" target="_blank" class="downloadIcon">
                 <object type="image/svg+xml" data="./svg/download.svg" class=""></object>
                 <p>Descargar Resume</p>
             </a>
