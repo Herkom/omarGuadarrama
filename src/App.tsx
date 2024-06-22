d415abf4ffec51f0b71710818be67d1e2c0ae47c
guadaro <omar.guadarrama@autodesk.com>
2024-06-21 21:03:40 -0600

    tailwind and fotter icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from './components/mode-toggle'
-import { Button } from "@/components/ui/button"
-import Gallery from './layouts/gallery'
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+import Gallery from "./layouts/gallery";
+import Footer from "./layouts/footer";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Taxing Laughter: The Joke Tax Chronicles
       </h1>
-      <ModeToggle/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <ModeToggle />
       <section className="flex justify-center">
         <Gallery />
       </section>
+      <Footer />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import "@fontsource/ubuntu/300.css";
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
