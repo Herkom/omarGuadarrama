245bc15ae146045f2f578cfadca4c592cd12aacf
guadaro <omar.guadarrama@autodesk.com>
2024-06-21 20:57:41 -0600

    font setup and styles format

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily:{
+      'sans': ['Ubuntu', '"Segoe UI"', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+    },
     container: {
       center: true,
       padding: "2rem",
