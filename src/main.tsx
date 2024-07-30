05c9d5c093305db05725c9013c59e6fb5e9dfa31
guadaro <omar.guadarrama@autodesk.com>
2024-07-30 12:41:51 -0600

    update path to test the deploy workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from '@/App'
+import '@/index.css'
 import "@fontsource/ubuntu/300.css";
 import "@fontsource/ubuntu";
 import "@fontsource/ubuntu/500.css";
